0fe4eba315f4ec58b6745dc452dbf3d373c6c66f
Ignacio Mercado <igmercado46@gmail.com>
2024-10-13 16:41:27 -0300

    adds register creation and displaying functionality

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Paquete } from '../models/Paquete.js';
+import { Registro } from '../models/Registro.js';
 
 const router = express.Router();
 
@@ -54,6 +55,27 @@ router.post('/crear-paquete', async (req, res) => {
 });
 
 // registros
+
+router.get('/ver-registros', async (req, res) => {
+    try {
+        const registrosExistentes = await Registro.findAll();
+        res.render('partials/registros/ver-registros', {
+            registros: registrosExistentes,
+            helpers: {
+                json: function (context) {
+                    return JSON.stringify(context, null, 2);
+                },
+                eq: function (v1, v2) {
+                    return v1 === v2;
+                }
+            }
+        });
+    } catch (error) {
+        console.error('Error al buscar registros:', error);
+        res.status(500).send('Error al buscar registros');
+    }
+});
+
 router.get('/crear-registro', async (req, res) => {
     try {
         const paquetesExistentes = await Paquete.findAll();
@@ -63,6 +85,20 @@ router.get('/crear-registro', async (req, res) => {
     } catch (error) {
         console.error('Error al abrir vista crear-registro:', error);
         res.status(500).send('Error al abrir vista crear-registro');
+    }
+});
+
+router.post('/crear-registro', async (req, res) => {
+
+    console.log('Datos recibidos:', req.body);
+    try {
+        const { id_paquete, alto, ancho, espesor, especie } = req.body;
+        const nuevoRegistro = await Registro.create({ id_paquete, alto, ancho, espesor, especie });
+        console.log('Nuevo registro creado:', JSON.stringify(nuevoRegistro, null, 2));
+        res.redirect('/ver-registros');
+    } catch (error) {
+        console.error('Error al crear el registro:', error);
+        res.status(500).send('Error al crear el registro');
     }
 });
 
