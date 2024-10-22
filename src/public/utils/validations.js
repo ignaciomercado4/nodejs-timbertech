f9c57446978dcdb5f86a822b9b23a8d753272345
Ignacio Mercado <igmercado46@gmail.com>
2024-10-22 16:36:11 -0300

    more registration from validations

--- src/public/utils/validations.js
+++ src/public/utils/validations.js
@@ -4,23 +4,41 @@ const FORM_REQUIREMENTS = {
     password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
 };
 
+const FORM_REQUIREMENTS_EXPLAINED = {
+    name: "El nombre debe tener entre 4 y 50 caracteres, usar solo letras y espacios",
+    email: "El email es inválido.",
+    password: "La contraseña debe tener al menos 8 caracteres, al menos una letra mayúscula y una minúscula, al menos un número y un caracter especial (#?!@$ %^&*-)."
+}
+
 function getFormElements() {
     const $form = document.querySelector('#registro-form');
     const formData = new FormData($form);
 
     return Object.fromEntries(formData);
+
+}
+
+function showErrors(errorsArray) {
+    console.log(errorsArray)
 }
 
 function validateRegistrationForm() {
     const formEntries = getFormElements();
+    delete formEntries["confirm-password"];
+
+    let errorsArray = [];
 
     for (const entry in formEntries) {
         if (FORM_REQUIREMENTS[entry] && FORM_REQUIREMENTS[entry].test(formEntries[entry])) {
-            console.log(entry, " pasa");
+            console.log(entry, ' pasa')
         } else {
-            console.log(entry, " no pasa");
+            errorsArray.push(FORM_REQUIREMENTS_EXPLAINED[entry]);
         }
     }
+
+    showErrors(errorsArray)
 }
+
+
 
 document.querySelector('#submit-button').onclick = validateRegistrationForm;
