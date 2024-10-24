b09bc87e0b14e0d57f9d6653778be7e46f8dfcb3
Ignacio Mercado <igmercado46@gmail.com>
2024-10-24 00:11:17 -0300

    finishes validations + adds feedback to resgistration form

--- src/public/utils/validations.js
+++ src/public/utils/validations.js
@@ -1,44 +1,56 @@
 const FORM_REQUIREMENTS = {
     name: /^[a-zA-Z\s]{4,50}$/,
     email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+    password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
 };
-
-const FORM_REQUIREMENTS_EXPLAINED = {
-    name: "El nombre debe tener entre 4 y 50 caracteres, usar solo letras y espacios",
-    email: "El email es inválido.",
-    password: "La contraseña debe tener al menos 8 caracteres, al menos una letra mayúscula y una minúscula, al menos un número y un caracter especial (#?!@$ %^&*-)."
-}
 
 function getFormElements() {
     const $form = document.querySelector('#registro-form');
     const formData = new FormData($form);
-
     return Object.fromEntries(formData);
-
 }
 
 function showErrors(errorsArray) {
-    console.log(errorsArray)
+    errorsArray.forEach((field) => {
+        const inputElement = document.getElementById(field);
+        inputElement.classList.add('is-invalid');
+    });
+}
+
+function clearErrors() {
+    document.querySelectorAll('.is-invalid').forEach((element) => {
+        element.classList.remove('is-invalid');
+    });
+}
+
+function validatePasswordsMatch() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    return password === confirmPassword;
 }
 
 function validateRegistrationForm() {
+    clearErrors();
     const formEntries = getFormElements();
     delete formEntries["confirm-password"];
 
     let errorsArray = [];
 
     for (const entry in formEntries) {
-        if (FORM_REQUIREMENTS[entry] && FORM_REQUIREMENTS[entry].test(formEntries[entry])) {
-            console.log(entry, ' pasa')
-        } else {
-            errorsArray.push(FORM_REQUIREMENTS_EXPLAINED[entry]);
+        if (FORM_REQUIREMENTS[entry] && !FORM_REQUIREMENTS[entry].test(formEntries[entry])) {
+            errorsArray.push(entry);
         }
     }
 
-    showErrors(errorsArray)
+    if (!validatePasswordsMatch()) {
+        errorsArray.push("confirm-password");
+    }
+
+    if (errorsArray.length === 0) {
+        document.querySelector('#registro-form').submit();
+    } else {
+        showErrors(errorsArray);
+    }
 }
-
-
 
 document.querySelector('#submit-button').onclick = validateRegistrationForm;
