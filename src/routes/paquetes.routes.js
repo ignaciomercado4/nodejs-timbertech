28e7fc9fab9feaea06d0ded1789ccc95bb1137ce
Ignacio Mercado <igmercado46@gmail.com>
2024-10-27 23:53:32 -0300

    completes filtering logic + remakes ui

--- src/routes/paquetes.routes.js
+++ src/routes/paquetes.routes.js
@@ -47,4 +47,71 @@ router.post('/crear-paquete', verifyToken, async (req, res) => {
     }
 });
 
+
+router.post('/filtrar-paquetes', verifyToken, async (req, res) => {
+    try {
+        const { filtrar_fecha_creacion, filtrar_turno } = req.body;
+        let paquetesFiltrados;
+
+        if (filtrar_fecha_creacion && filtrar_turno) {
+            paquetesFiltrados = await Paquete.findAll({
+                where: {
+                    shift: filtrar_turno,
+                },
+                order: [['date', filtrar_fecha_creacion]],
+            });
+        } else if (filtrar_fecha_creacion && !filtrar_turno) {
+            paquetesFiltrados = await Paquete.findAll({
+                order: [['date', filtrar_fecha_creacion]],
+            });
+        } else if (filtrar_turno && !filtrar_fecha_creacion) {
+            paquetesFiltrados = await Paquete.findAll({
+                where: {
+                    shift: filtrar_turno,
+                }
+            });
+        } else {
+            return res.redirect('/ver-paquetes');
+        }
+
+        res.render('partials/paquetes/ver-paquetes', {
+            paquetes: paquetesFiltrados,
+            helpers: {
+                json: function (context) {
+                    return JSON.stringify(context, null, 2);
+                },
+                eq: function (v1, v2) {
+                    return v1 === v2;
+                }
+            }
+        });
+
+    } catch (error) {
+        console.error('Error al aplicar filtros a los paquetes:', error);
+        res.status(500).send('Error al aplicar filtros a los paquetes');
+    }
+});
+
+router.get('/quitar-filtros', verifyToken, async (req, res) => {
+    try {
+        const paquetes = await Paquete.findAll();
+
+        res.render('partials/paquetes/ver-paquetes', {
+            paquetes: paquetes,
+            helpers: {
+                json: function (context) {
+                    return JSON.stringify(context, null, 2);
+                },
+                eq: function (v1, v2) {
+                    return v1 === v2;
+                }
+            }
+        });
+
+    } catch (error) {
+        console.error('Error al quitar filtros:', error);
+        res.status(500).send('Error al quitar filtros');
+    }
+});
+
 export default router;
